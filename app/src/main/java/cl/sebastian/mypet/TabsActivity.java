ce2149426b231747f0f38d0387f1364dc43be97d
Sebastian <smenap@gmail.com>
2017-08-03 11:39:18 -0400

    Correciones ToolBar.

--- app/src/main/java/cl/sebastian/mypet/TabsActivity.java
+++ app/src/main/java/cl/sebastian/mypet/TabsActivity.java
@@ -1,27 +1,21 @@
 package cl.sebastian.mypet;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
-import android.widget.Toast;
-
-import static java.security.AccessController.getContext;
 
 public class TabsActivity extends AppCompatActivity {
 
@@ -85,16 +79,17 @@ public class TabsActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.dog) {
-            Toast.makeText(TabsActivity.this,"Guau Guau",Toast.LENGTH_SHORT).show();
-            return true;
-        }else if (id == R.id.cat) {
-            Toast.makeText(TabsActivity.this,"Miauuuuuu",Toast.LENGTH_SHORT).show();
-            return true;
-        }else if (id == R.id.love) {
-            Toast.makeText(TabsActivity.this,"Amor por los Animales",Toast.LENGTH_SHORT).show();
-            return true;
-        }
+//        if (id == R.id.dog) {
+//            Toast.makeText(TabsActivity.this,"Guau Guau",Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+//        else if (id == R.id.cat) {
+//            Toast.makeText(TabsActivity.this,"Miauuuuuu",Toast.LENGTH_SHORT).show();
+//            return true;
+//        }else if (id == R.id.love) {
+//            Toast.makeText(TabsActivity.this,"Amor por los Animales",Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
